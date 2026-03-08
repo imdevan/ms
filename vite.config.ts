cade03f7c6f5d4f69e345c378eecd966da659ada
Devan Huapaya <huapayadevan@gmail.com>
2026-03-08 16:25:38 -0700

    update prod base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/meassuring-spoon/" : "/",
+  base: mode === "production" ? "/ms/" : "/",
   server: {
     host: "::",
     port: 8080,
